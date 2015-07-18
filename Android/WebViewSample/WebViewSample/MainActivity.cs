b463e1aa6ac2ff1c6c7ab45bd30effe6c69d3cb6
snowmoun <snowmoun114@gmail.com>
2015-07-19 07:59:22 +0800

    WebView Sample

---
+++ Android/WebViewSample/WebViewSample/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace WebViewSample
+{
+    [Activity(Label = "棟哥的部落格", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        WebView webView;
+        string webViewURL = "http://www.adminote.com";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            webView = FindViewById<WebView>(Resource.Id.webView);
+
+            webView.Settings.JavaScriptEnabled = true;
+
+            webView.LoadUrl(webViewURL);
+        }
+    }
+}
+
